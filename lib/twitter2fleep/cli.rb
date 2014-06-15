cb8bb26441cc77157b969e344674d5059da2a5ef
WanCW <wancw.wang@gmail.com>
2014-06-16 00:50:46 +0800

    Resolve top-level module dependency.

--- lib/twitter2fleep/cli.rb
+++ lib/twitter2fleep/cli.rb
@@ -3,51 +3,53 @@ require 'twitter2fleep/bot'
 require 'thor'
 require 'yaml'
 
-class Twitter2Fleep::CLI < Thor
-  desc "start", "Start the bot"
-  option :config_file, :default => "#{Dir.home}/.twitter2fleep/config.yml"
-  option :env_config, :type => :boolean, :default => false
+module Twitter2Fleep
+  class CLI < Thor
+    desc "start", "Start the bot"
+    option :config_file, :default => "#{Dir.home}/.twitter2fleep/config.yml"
+    option :env_config, :type => :boolean, :default => false
 
-  def start
-    if options[:env_config]
-      puts "Load config from environment variables."
-      config = load_env_config
-    else
-      config_file = options[:config_file]
-      puts "Load config from \`#{config_file}\`."
-      config = load_config_file(config_file)
-    end
-    bot = Twitter2Fleep::Bot.new(config)
-    bot.start
-  end
-
-  no_commands do
-    def load_env_config
-      {
-        :twitter => {
-          :consumer_key => ENV.fetch('TWITTER_CONSUMER_KEY', ''),
-          :consumer_secret => ENV.fetch('TWITTER_CONSUMER_SECRET', ''),
-          :access_token => ENV.fetch('TWITTER_ACCESS_TOKEN', ''),
-          :access_token_secret => ENV.fetch('TWITTER_ACCESS_TOKEN_SECRET', ''),
-        },
-        :selected_user_ids => ENV.fetch('SELECTED_USER_IDS', '').split(',').map {|v| v.to_i},
-        :fleep_hook_url => ENV.fetch('FLEEP_HOOK_URL', '')
-      }
+    def start
+      if options[:env_config]
+        puts "Load config from environment variables."
+        config = load_env_config
+      else
+        config_file = options[:config_file]
+        puts "Load config from \`#{config_file}\`."
+        config = load_config_file(config_file)
+      end
+      bot = Twitter2Fleep::Bot.new(config)
+      bot.start
     end
 
-    def load_config_file(filename)
-      config_file = options[:config_file]
-
-      begin
-        config = YAML::load_file(config_file)
-      rescue Errno::ENOENT => exception
-        puts "You need to create the config file \`#{config_file}\`"
-        puts "See example at https://github.com/wancw/twitter2fleep/blob/master/config.yml.example"
-        exit(-1)
+    no_commands do
+      def load_env_config
+        {
+          :twitter => {
+            :consumer_key => ENV.fetch('TWITTER_CONSUMER_KEY', ''),
+            :consumer_secret => ENV.fetch('TWITTER_CONSUMER_SECRET', ''),
+            :access_token => ENV.fetch('TWITTER_ACCESS_TOKEN', ''),
+            :access_token_secret => ENV.fetch('TWITTER_ACCESS_TOKEN_SECRET', ''),
+          },
+          :selected_user_ids => ENV.fetch('SELECTED_USER_IDS', '').split(',').map {|v| v.to_i},
+          :fleep_hook_url => ENV.fetch('FLEEP_HOOK_URL', '')
+        }
       end
 
-      return config
-    end
-  end
+      def load_config_file(filename)
+        config_file = options[:config_file]
 
+        begin
+          config = YAML::load_file(config_file)
+        rescue Errno::ENOENT => exception
+          puts "You need to create the config file \`#{config_file}\`"
+          puts "See example at https://github.com/wancw/twitter2fleep/blob/master/config.yml.example"
+          exit(-1)
+        end
+
+        return config
+      end
+    end
+
+  end
 end
